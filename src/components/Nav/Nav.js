6ef585890389efcc51bb6bde941481e7c350d1bc
Emmanuel <mailkumah@gmail.com>
2022-07-07 11:35:58 +0000

    Start responsiveness

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./Nav.module.css";
 import { MdOutlineDeliveryDining } from "react-icons/md";
 
 function Nav() {
+  const [isSticky, setIsSticky] = useState(false);
+
+  function stickyNavBar() {
+    let windowHeight = window.scrollY;
+    console.log(windowHeight);
+    windowHeight > 200 ? setIsSticky(true) : setIsSticky(false);
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", stickyNavBar);
+  }, []);
+
   return (
     <>
-      <nav className={classes["nav_container"]} id="nav">
+      <nav
+        className={`${classes["nav_container"]} ${
+          isSticky && classes["sticky"]
+        }`}
+        id="nav"
+      >
         <div className={classes["nav_brand"]}>
           <h1>Delvry Guy</h1>
           <MdOutlineDeliveryDining className={classes["nav_icon"]} />
