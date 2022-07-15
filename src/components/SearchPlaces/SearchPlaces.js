e2939dcde27ce2939b4106a4c5f12c43208e2828
Emmanuel <mailkumah@gmail.com>
2022-07-15 13:37:25 +0000

    Complete data fetching from api

--- src/components/SearchPlaces/SearchPlaces.js
+++ src/components/SearchPlaces/SearchPlaces.js
@@ -1,37 +1,77 @@
 import React, { useEffect, useState } from "react";
-import Notification from "./Notification";
+import axios from "axios";
 
 function SearchPlaces() {
   const [geoLocation, setGeoLocation] = useState({
-    latitdue: "",
+    latitude: "",
     longitude: "",
   });
-  const [errorMsg, setErrorMsg] = useState({});
+  const [errorMsg, setErrorMsg] = useState("");
+  const [enteredPlace, setEnteredPlace] = useState("");
   //   const [searchResults, setSearchResults] = useState({});
+  const apiKey = process.env.REACT_APP_KEY;
+
+  //get nearby places
+
+  async function getNearbyPlaces(
+    enteredPlace,
+    lat,
+    long,
+    limit = 5,
+    radius = 10000
+  ) {
+    let baseUrl = "https://api.tomtom.com/search/2/poiSearch";
+    let queryString = `limit=${limit}&lat=${lat}&lon=${long}&radius=${radius}&key=${apiKey}`;
+    try {
+      const response = await axios.get(
+        `${baseUrl}/${enteredPlace}.json?${queryString}`
+      );
+      console.log(response.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (e) => {
-        console.log("cords", e.coords);
-        setGeoLocation((prevState) => {
-          return {
-            ...prevState,
-            latitdue: e.coords.latitude,
-            longitude: e.coords.longitude,
-          };
-        });
-        //set errorMsg
+        setGeoLocation((prevState) => ({
+          ...prevState,
+          latitude: e.coords.latitude,
+          longitude: e.coords.longitude,
+        }));
       },
       async (err) => {
-        setErrorMsg({ err });
+        setErrorMsg(err);
       }
     );
-    console.log("err", errorMsg);
-  }, []);
+    getNearbyPlaces(enteredPlace, geoLocation.latitude, geoLocation.longitude);
+  }, [enteredPlace]);
+
   return (
     <>
       <h1>Place search</h1>
-      <Notification geoLocation={geoLocation} errorMsg={errorMsg} />
+      <div>
+        {errorMsg ? (
+          <p>{errorMsg}</p>
+        ) : (
+          <p>
+            <span>{geoLocation.longitude}</span>
+            <span>{geoLocation.latitude}</span>
+          </p>
+        )}
+      </div>
+      <section>
+        <form onSubmit={getNearbyPlaces}>
+          <input
+            type="text"
+            value={enteredPlace}
+            onChange={(e) => setEnteredPlace(e.target.value)}
+          />
+          <p>{enteredPlace}</p>
+          <button>Search Place</button>
+        </form>
+      </section>
     </>
   );
 }
